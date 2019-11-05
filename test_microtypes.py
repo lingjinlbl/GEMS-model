0e00d5fccdaf74810778db9834bef8d4dfd07561
Zach Needell <zaneedell@lbl.gov>
2019-11-05 11:05:08 -0800

    second plot kinda working

--- test_microtypes.py
+++ test_microtypes.py
@@ -7,8 +7,8 @@ from utils.microtype import Microtype
 
 network_params_default = io.Network(0.068, 15.42, 1.88, 0.145, 0.177, 1000, 50)
 bus_params_default = io.BusParams(mean_trip_distance=1000, road_network_fraction=500, relative_length=3.0,
-                                  fixed_density=85. / 100., min_stop_time=10., stop_spacing=1. / 250.,
-                                  passenger_wait=4.)
+                                  fixed_density=95. / 100., min_stop_time=15., stop_spacing=1. / 250.,
+                                  passenger_wait=5.)
 
 car_params_default = io.ModeParams(mean_trip_distance=1000, relative_length=1.0)
 
@@ -18,11 +18,12 @@ modeCharacteristics['bus'] = io.ModeCharacteristics('bus', bus_params_default, d
 
 m = Microtype(network_params_default, modeCharacteristics)
 
-car_demands = np.arange(0.05, 0.12, 0.0015)
-bus_demands = np.arange(0.001, 0.06, 0.001)
+car_demands = np.arange(0.05, 0.13, 0.001)
+bus_demands = np.arange(0.001, 0.08, 0.001)
 
-averageCosts = np.zeros((np.size(car_demands), np.size(bus_demands)))
+average_costs = np.zeros((np.size(car_demands), np.size(bus_demands)))
 flows = np.zeros((np.size(car_demands), np.size(bus_demands)))
+car_speeds = np.zeros((np.size(car_demands), np.size(bus_demands)))
 
 for ii in range(np.size(car_demands)):
     for jj in range(np.size(bus_demands)):
@@ -31,8 +32,12 @@ for ii in range(np.size(car_demands)):
         m = Microtype(network_params_default, modeCharacteristics)
         m.findEquilibriumDensityAndSpeed()
         flows[ii, jj] = np.sum(m.getFlows())
-        averageCosts[ii, jj] = np.sum(m.getTotalTimes()) / np.sum(m.getFlows())
-p1 = plt.contourf(bus_demands, car_demands, averageCosts, np.arange(0.08, 0.20, 0.01))
+        car_speeds[ii, jj] = m.getModeSpeed('car')
+        average_costs[ii, jj] = np.sum(m.getTotalTimes()) / np.sum(m.getFlows())
+
+fig1 = plt.figure(figsize=(8, 5))
+
+p1 = plt.contourf(bus_demands, car_demands, average_costs)  # , np.arange(0.08, 0.35, 0.02))
 p2 = plt.contour(bus_demands, car_demands, flows, np.arange(45, 200, 10), cmap='Greys')
 cb1 = plt.colorbar(p1)
 cb2 = plt.colorbar(p2)
@@ -43,33 +48,50 @@ cb2.set_label('Total Passenger Flow')
 plt.xlabel('Bus Demand (trips / time)')
 plt.ylabel('Car Demand (trips / time)')
 
-g1 = np.gradient(averageCosts)
+g1 = np.gradient(average_costs)
 g2 = np.gradient(flows)
 
 p3 = plt.contour(bus_demands, car_demands, g1[0] / g1[1] - g2[0] / g2[1], 0, linestyles='dashed', linewidths=2,
                  cmap='Reds')
-"""
-bus_params_default = {'k': 85. / 100.,
-                      't_0': 10,
-                      's_b': 1. / 250.,
-                      'gamma_s': 4.,
-                      'size': 3.0,
-                      'meanTripDistance': 1000,
-                      'L_mode': 500
-                      }
-mode_params_default = {'car': car_params_default, 'bus': bus_params_default}
+
+
+
 
 totalDemand = 0.16
 portions = np.arange(0.5, 1.0, 0.02)
 oneDemandCosts = np.zeros(np.shape(portions))
 
-for ii in range(np.size(portions)):
-    demands = {'car': portions[ii] * totalDemand, 'bus': (1. - portions[ii]) * totalDemand}
-    m2 = microtype.Microtype(modes, mode_params_default, network_params_default, demands)
-    m2.findEquilibriumDensityAndSpeed()
-    oneDemandCosts[ii] = np.sum(m2.getTotalTimes()) / np.sum(m2.getFlows())
+bus_params = io.BusParams(mean_trip_distance=1000, road_network_fraction=500, relative_length=3.0,
+                                  fixed_density=95. / 100., min_stop_time=15., stop_spacing=1. / 250.,
+                                  passenger_wait=5.)
+modeCharacteristics['bus'] = io.ModeCharacteristics('bus', bus_params, demand=17 / (10 * 60))
 
+for ii in range(np.size(portions)):
+
+    modeCharacteristics.setModeDemand('car',  portions[ii] * totalDemand)
+    modeCharacteristics.setModeDemand('bus', (1. - portions[ii]) * totalDemand)
+    m = Microtype(network_params_default, modeCharacteristics)
+    m.findEquilibriumDensityAndSpeed()
+    oneDemandCosts[ii] = np.sum(m.getTotalTimes()) / np.sum(m.getFlows())
+
+totalDemand = 0.16
+portions = np.arange(0.5, 1.0, 0.02)
+oneDemandCosts2 = np.zeros(np.shape(portions))
+
+bus_params = io.BusParams(mean_trip_distance=1000, road_network_fraction=500, relative_length=3.0,
+                                  fixed_density=85. / 100., min_stop_time=15., stop_spacing=1. / 250.,
+                                  passenger_wait=5.)
+modeCharacteristics['bus'] = io.ModeCharacteristics('bus', bus_params, demand=17 / (10 * 60))
+
+fig2 = plt.figure(figsize=(8, 5))
 plt.plot(portions, oneDemandCosts)
 
+for ii in range(np.size(portions)):
 
-"""
+    modeCharacteristics.setModeDemand('car',  portions[ii] * totalDemand)
+    modeCharacteristics.setModeDemand('bus', (1. - portions[ii]) * totalDemand)
+    m = Microtype(network_params_default, modeCharacteristics)
+    m.findEquilibriumDensityAndSpeed()
+    oneDemandCosts2[ii] = np.sum(m.getTotalTimes()) / np.sum(m.getFlows())
+
+plt.plot(portions, oneDemandCosts2)
