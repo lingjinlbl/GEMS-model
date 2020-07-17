fd869d90d68ac4e0c1c0f28a186e3d95b341b7ce
Zach Needell <zaneedell@lbl.gov>
2020-07-17 16:11:52 -0700

    mode split test

---
+++ tests/test_population.py
@@ -0,0 +1,34 @@
+import pytest
+from utils.population import Population
+from utils.choiceCharacteristics import ModalChoiceCharacteristics, ChoiceCharacteristics
+import pandas as pd
+
+
+@pytest.fixture
+def pop():
+    return Population()
+
+
+def test_import_population(pop):
+    popdata = pd.read_csv("../input-data-simpler/Population.csv")
+    popgroups = pd.read_csv("../input-data-simpler/PopulationGroups.csv")
+    pop.importPopulation(popdata, popgroups)
+    assert True
+    return pop
+
+
+def test_update_mode_split(pop):
+    pop = test_import_population(pop)
+    mcc = ModalChoiceCharacteristics([])
+    mcc["auto"] = ChoiceCharacteristics(0.5, 10, 0)  # Expensive, should be preferred by high VOT
+    mcc["bus"] = ChoiceCharacteristics(1.0, 0, 0)
+
+    dis = []
+    ms = []
+
+    for di, dc in pop:
+        dis.append(di)
+        ms.append(dc.updateModeSplit(mcc))
+        print("AAH")
+
+    assert ms[0]["auto"] > ms[1]["auto"]  # Rich people are more likely to drive
