fixed units, more small fixes

model.py
@@ -73,7 +73,7 @@ class Optimizer:
         userCosts, operatorCosts = self.model.collectAllCosts()
         dedicationCosts = self.getDedicationCost(reallocations)
         print(reallocations)
-        print(userCosts.total + operatorCosts.total + dedicationCosts)
+        print(userCosts.total, operatorCosts.total, dedicationCosts)
         return userCosts.total + operatorCosts.total + dedicationCosts
 
     def getBounds(self):
@@ -234,7 +234,7 @@ class Model:
         self.__originDestination.initializeTimePeriod(timePeriod)
         self.__tripGeneration.initializeTimePeriod(timePeriod)
         self.demand.initializeDemand(self.__population, self.__originDestination, self.__tripGeneration, self.__trips,
-                                     self.microtypes, self.__distanceBins, 1.1)
+                                     self.microtypes, self.__distanceBins, 0.7)
         self.choice.initializeChoiceCharacteristics(self.__trips, self.microtypes, self.__distanceBins)
 
     def findEquilibrium(self):
@@ -242,17 +242,18 @@ class Model:
         i = 0
         while (diff > 0.00001) & (i < 20):
             ms = self.getModeSplit()
-            self.demand.updateMFD(self.microtypes, 7)
+            self.demand.updateMFD(self.microtypes, 5)
             self.choice.updateChoiceCharacteristics(self.microtypes, self.__trips)
             diff = self.demand.updateModeSplit(self.choice, self.__originDestination, ms)
-            c = self.getOperatorCosts().total
-            if np.isnan(c):
-                print("----")
-
-            c2 = self.getUserCosts().total
-            if np.isnan(c2):
-                print("----")
-            i += 1
+            # c = self.getOperatorCosts().total
+            # if np.isnan(c):
+            #     print("----")
+            #
+            # c2 = self.getUserCosts().total
+            # if np.isnan(c2):
+            #     print("----")
+            # i += 1
+        ms = self.getModeSplit()
 
     def getModeSplit(self, timePeriod=None):
         if timePeriod is None:
@@ -301,6 +302,10 @@ class Model:
 
 
 if __name__ == "__main__":
+    # a = Model("input-data-production")
+    # a.initializeTimePeriod("morning_rush")
+    # a.findEquilibrium()
+    # ms = a.getModeSplit()
     # o = Optimizer("input-data", list(zip([2, 4, 6, 8], [13, 14, 15, 16])))
     # o = Optimizer("input-data", fromToSubNetworkIDs=list(zip([2, 8], [13, 16])),
     #               modesAndMicrotypes=list(zip(["A", "D", "A", "D"], ["bus", "bus", "rail", "rail"])),
@@ -308,16 +313,16 @@ if __name__ == "__main__":
     o = Optimizer("input-data",
                   modesAndMicrotypes=list(zip(["A", "B", "C", "D"], ["bus", "bus", "bus", "bus"])),
                   method="noisy")
-    # o.evaluate(np.array([0., 30., 200., 200., 300., 300.]))
-    # o.evaluate(np.array([0., 30., 200., 200., 300., 300.]))
-    # o.evaluate(np.array([300., 200., 200., 200.]))
+    # # o.evaluate(np.array([0., 30., 200., 200., 300., 300.]))
+    # # o.evaluate(np.array([0., 30., 200., 200., 300., 300.]))
+    # # o.evaluate(np.array([300., 200., 200., 200.]))
     output = o.minimize()
     print("DONE")
-    print(output.x)
-    print(output.fun)
-    print(output.message)
-
-    print("DONE")
+    # print(output.x)
+    # print(output.fun)
+    # print(output.message)
+    #
+    # print("DONE")
     # cost2 = o.evaluate(np.array([10., 1.]))
     # print(cost2)
     # cost3 = o.evaluate(np.array([500., 10.]))

tests/test_network.py
@@ -1,7 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 import pytest
-
-from utils.network import Network, NetworkFlowParams, AutoMode, BusMode, TravelDemand
+import matplotlib.tri as tri
+from utils.microtype import Microtype
+from utils.network import Network, NetworkFlowParams, AutoMode, BusMode, TravelDemand, NetworkCollection
 
 
 @pytest.fixture
@@ -35,3 +38,51 @@ def test_mfd(net):
     net.MFD()
     bs4 = net.getBaseSpeed()
     assert bs4 < bs3
+
+    net2 = Network(pd.DataFrame({"Length": {0: 6000}, "Dedicated": {0: True}}), 0.,
+                   NetworkFlowParams(0.068, 15.42, 1.88, 0.145, 0.177, 50))
+    net3 = Network(pd.DataFrame({"Length": {0: 11000}, "Dedicated": {0: True}}), 0.,
+                   NetworkFlowParams(0.068, 15.42, 1.88, 0.145, 0.177, 50))
+
+    autoData = pd.DataFrame({"VehicleSize": 1}, index=["A"])
+    busData = pd.DataFrame(
+        {"VehicleSize": 3, "Headway": 600, "PassengerWait": 5, "PassengerWaitDedicated": 2., "MinStopTime": 15.,
+         "PerStartCost": 2.5, "VehicleOperatingCostPerHour": 30., "StopSpacing": 300}, index=["A"])
+
+    modeToModeData = {"auto": autoData, "bus": busData}
+    subNetworkToModes = {net2: ["auto", "bus"], net3: ["auto"]}
+    networkCollection = NetworkCollection(subNetworkToModes, modeToModeData, "A")
+    m = Microtype("A", networkCollection)
+
+    busTrips = np.arange(1000, 20000, 1000.0)
+    autoTrips = np.arange(200, 4000, 200.0)
+    out = np.zeros((len(busTrips), len(autoTrips)))
+    autoAccumulation = []
+    busAccumulation = []
+    production = []
+    speeds = []
+    occupancy = []
+    for i in range(len(busTrips)):
+        for j in range(len(autoTrips)):
+            m.resetDemand()
+            # networkCollection.updateNetworks()
+            m.addModeDemandForPMT("bus", busTrips[i], 1000)
+            m.addModeStarts("bus", busTrips[i])
+            m.addModeDemandForPMT("auto", autoTrips[j], 2000)
+            networkCollection.updateModes()
+            print(networkCollection._networks[0].N_eq)
+            out[i, j] = m.getModeSpeed("auto")
+            speeds.append(m.getModeSpeed("bus"))
+            production.append(networkCollection.modes['auto']._N_tot * m.getModeSpeed("auto") + networkCollection.modes[
+                'bus']._N_tot * m.getModeSpeed("bus"))
+            autoAccumulation.append(networkCollection.modes['auto']._N_tot)
+            busAccumulation.append(networkCollection.modes['bus']._N_tot)
+            occupancy.append(networkCollection.modes['bus'].getOccupancy())
+
+
+    triang = tri.Triangulation(autoAccumulation, busAccumulation)
+    interpolator = tri.LinearTriInterpolator(triang, speeds)
+    Xi, Yi = np.meshgrid(np.linspace(0,max(autoAccumulation),50), np.linspace(0,max(busAccumulation),50))
+    zi = interpolator(Xi, Yi)
+    plt.contourf(Xi, Yi, zi)
+    print("AH")

utils/network.py
@@ -362,7 +362,7 @@ class BusMode(Mode):
             out = network.avgLinkLength / (
                     self.minStopTimeInSec + self.headwayInSec * perPassenger * (
                     self.travelDemand.tripStartRatePerHour + self.travelDemand.tripEndRatePerHour) / (
-                            self.routeLength / self.stopSpacingInMeters)) / self.headwayInSec
+                            self.routeLength / self.stopSpacingInMeters * 3600.)) / self.headwayInSec
             # busSpeed = self.getSubNetworkSpeed(network.car_speed)
             # out = busSpeed / self.stop_spacing * self.N_tot * self.min_stop_time * network.l
         else:
@@ -398,7 +398,7 @@ class BusMode(Mode):
         #     print("AAAH")
 
     def getOccupancy(self) -> float:
-        return self.travelDemand.averageDistanceInSystemInMiles / self.routeAveragedSpeed * self.travelDemand.tripStartRatePerHour / self._N_tot
+        return self.travelDemand.averageDistanceInSystemInMiles / (self.routeAveragedSpeed * 2.23694) * self.travelDemand.tripStartRatePerHour / self._N_tot
 
     def getPassengerFlow(self) -> float:
         if np.any([n.isJammed for n in self._networks]):
@@ -436,7 +436,7 @@ class Network:
 
     @property
     def L(self):
-        return self.data.at[self.__idx, "Length"]
+        return self.data.loc[self.__idx, "Length"]
 
     @L.setter
     def L(self, L):