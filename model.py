8b6248ed6890d9d629952d7f205859b05d7e1e91
Zach Needell <zaneedell@lbl.gov>
2020-08-14 13:30:25 -0700

    remove print statement

--- model.py
+++ model.py
@@ -255,8 +255,8 @@ class Model:
             # if np.isnan(c2):
             #     print("----")
             # i += 1
-            print(ms)
-            print(self.getModeSpeeds().loc['auto', ['A_1', 'A_2', 'A_4', 'B_1', 'B_2', 'B_4']])
+            # print(ms)
+            # print(self.getModeSpeeds().loc['auto', ['A_1', 'A_2', 'A_4', 'B_1', 'B_2', 'B_4']])
         ms = self.getModeSplit()
 
     def getModeSplit(self, timePeriod=None):
