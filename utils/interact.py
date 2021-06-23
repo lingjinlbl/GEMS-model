ceded333a3f1596ec7181681cf40fdbd3eb678ea
Zach Needell <zaneedell@lbl.gov>
2021-06-23 12:38:43 -0700

    simplify

--- utils/interact.py
+++ utils/interact.py
@@ -197,4 +197,3 @@ class Interact:
                 handle.visible = True
             self.fig.layout.xaxis.title.text = "Time"
             self.fig.layout.yaxis.title.text = "Portion of trips"
-        self.__fig.show()
